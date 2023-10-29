b80703e1f4f77fc589756fb372e75e4cb83a0170
botamochi6277 <botamochi6277@icloud.com>
2023-10-29 19:21:05 +0900

    add api mode

--- file_server.js
+++ file_server.js
@@ -13,6 +13,12 @@ const args = yargs
     demandOption: true,
     default: 8002
   })
+  .option('mode', {
+    alias: 'm',
+    description: 'mode, page/api',
+    demandOption: true,
+    default: "page"
+  })
   .help()
   .argv
 
@@ -25,23 +31,34 @@ function getIpAddress() {
 const host = getIpAddress();
 
 const app = express();
-// const args = process.argv.slice(2);
 const rootPath = args._[0];
 app.use(express.static(rootPath, { index: false }));
 
-//index.htmlを勝手に見に行かない指定
-
 const serveIndex = require('serve-index');
 
-app.use(serveIndex(rootPath, {
-  icons: true,
-  view: 'details',//'tiles',
-  template: './template.html',
-}));
+if (args.mode === "page") {
+  app.use(serveIndex(rootPath, {
+    icons: true,
+    view: 'details',//'tiles',
+    template: './template.html',
+  }));
+} else if (args.mode === "api") {
+  app.use(serveIndex(rootPath, {
+    template: (locals, callback) => {
+      callback(null, JSON.stringify({
+        directory: locals.directory,
+        files: locals.fileList
+      }));
+    },
+  }));
+} else {
+  // 
+  throw new Error(`mode must be page or api. this is ${args.mode}`);
+}
 
 const hostname = os.hostname();
 const port = args.port
 app.listen(port, () => {
-  console.log(`File Server for ${rootPath}`);
+  console.log(`File Server is running for ${rootPath} in ${args.mode} mode`);
   console.log(`Please access http://${hostname}:${port}`);
 });
